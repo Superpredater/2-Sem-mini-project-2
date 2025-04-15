c1dcdee959a64078ea9341899c7c6d8a3f46c0dd
Teju192006 <teju192006@gmail.com>
2025-04-15 21:31:29 +0530

    Create server.sql

---
+++ server.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE ims_db;
+
+USE ims_db;
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(100) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  role VARCHAR(50),
+  profile_pic TEXT
+);
